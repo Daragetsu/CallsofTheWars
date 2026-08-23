8b48f3bb7efe2312f536bf319c431b23ddb117e5
Daragetsu <nabbonoushad@gmail.com>
2026-08-24 04:35:48 +0600

    loot table

--- src/main/java/com/daragetsu/callsofthewars/entities/common/util/EnlistHandler.java
+++ src/main/java/com/daragetsu/callsofthewars/entities/common/util/EnlistHandler.java
@@ -7,17 +7,22 @@ import com.daragetsu.callsofthewars.entities.ModEntities;
 import com.daragetsu.callsofthewars.entities.container.ContainerEntity;
 import com.daragetsu.callsofthewars.item.ModItems;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.ServerScoreboard;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity.RemovalReason;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.scores.Objective;
 import net.minecraft.world.scores.criteria.ObjectiveCriteria;
@@ -55,17 +60,24 @@ public class EnlistHandler {
         scoreboard.getOrCreatePlayerScore(player.getName().getString(), x).setScore((int)player.getX());
         scoreboard.getOrCreatePlayerScore(player.getName().getString(), y).setScore((int)player.getY());
         scoreboard.getOrCreatePlayerScore(player.getName().getString(), z).setScore((int)player.getZ());
+        
         ContainerEntity conen = new ContainerEntity(ModEntities.CONTAINER_ENTITY.get(), player.level());
         conen.moveTo(player.getX(), player.getY(), player.getZ());
         conen.setContainerOwner(player.getName().getString());
         conen.serializeInventory(player);
         player.level().addFreshEntity(conen);
+        
         player.getInventory().clearContent();
-        player.getInventory().add(0, new ItemStack(top.ribs.scguns.init.ModItems.BIRDFEEDER.get()));
-        player.getInventory().add(1, new ItemStack(top.ribs.scguns.init.ModItems.STANDARD_COPPER_ROUND.get(), 64));
-        player.getInventory().add(2, new ItemStack(top.ribs.scguns.init.ModItems.STANDARD_COPPER_ROUND.get(), 64));
-        player.getInventory().add(3, new ItemStack(top.ribs.scguns.init.ModItems.STANDARD_COPPER_ROUND.get(), 64));
-        player.getInventory().add(4, new ItemStack(Items.GOLDEN_CARROT, 64));
+        LootTable STARTER_INVENTORY = player.server.getLootData().getLootTable(ResourceLocation.fromNamespaceAndPath(CallsofTheWars.MOD_ID, "starter_inventory"));
+        LootParams.Builder builder = new LootParams.Builder((ServerLevel) player.level());
+        builder.withParameter(LootContextParams.ORIGIN, player.position());
+        builder.withLuck(player.getLuck());
+        LootParams params = builder.create(LootContextParamSets.EMPTY);
+        ObjectArrayList<ItemStack> items = STARTER_INVENTORY.getRandomItems(params);
+
+        for(ItemStack item : items){
+            player.getInventory().add(item);
+        }
 
         player.getInventory().add(36, new ItemStack(top.ribs.scguns.init.ModItems.ANTHRALITE_BOOTS.get()));
         player.getInventory().add(37, new ItemStack(top.ribs.scguns.init.ModItems.ANTHRALITE_LEGGINGS.get()));
@@ -74,6 +86,7 @@ public class EnlistHandler {
 
         player.teleportTo(player.serverLevel().getServer().getLevel(ResourceKey.create(Registries.DIMENSION, ResourceLocation.fromNamespaceAndPath(CallsofTheWars.MOD_ID, "warring_states"))), 5000, 100, 5000, 0, 0);
         player.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 200));
+        
         int i = player.getRandom().nextInt(3);
         switch (i) {
             case 0:
