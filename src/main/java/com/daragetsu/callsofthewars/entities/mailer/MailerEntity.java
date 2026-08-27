1bc54f96f4be17ba547d1f9d426489f979d3000d
Daragetsu <nabbonoushad@gmail.com>
2026-08-27 21:21:04 +0600

    less commands

--- src/main/java/com/daragetsu/callsofthewars/entities/mailer/MailerEntity.java
+++ src/main/java/com/daragetsu/callsofthewars/entities/mailer/MailerEntity.java
@@ -1,9 +1,12 @@
 package com.daragetsu.callsofthewars.entities.mailer;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.Goal;
@@ -24,7 +27,7 @@ import software.bernie.geckolib.core.animation.AnimationController;
 import software.bernie.geckolib.core.animation.RawAnimation;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class MailerEntity extends Monster implements GeoEntity{
+public class MailerEntity extends Monster implements GeoEntity {
 
     private final AnimatableInstanceCache geocache = GeckoLibUtil.createInstanceCache(this);
 
@@ -36,12 +39,12 @@ public class MailerEntity extends Monster implements GeoEntity{
     public void registerControllers(ControllerRegistrar controllers) {
         controllers.add(new AnimationController<>(this, "controller", 2,
                 state -> {
-                    if(((this.getX() - this.xo)*(this.getX() - this.xo))+((this.getZ() - this.zo)*(this.getZ() - this.zo))>0.0002){
+                    if (((this.getX() - this.xo) * (this.getX() - this.xo))
+                            + ((this.getZ() - this.zo) * (this.getZ() - this.zo)) > 0.0002) {
                         return state.setAndContinue(RawAnimation.begin().thenLoop("walk"));
                     }
                     return state.setAndContinue(RawAnimation.begin().thenLoop("idle"));
-                }
-        ));
+                }));
     }
 
     @Override
@@ -50,57 +53,62 @@ public class MailerEntity extends Monster implements GeoEntity{
         this.goalSelector.addGoal(0, new FloatGoal(this));
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, false));
         this.goalSelector.addGoal(2, new MoveTowardsTargetGoal(this, 0.4, 50));
-        this.goalSelector.addGoal(3, new Goal() {//can't bother making a new class
+        this.goalSelector.addGoal(3, new Goal() {// can't bother making a new class
             @Override
             public boolean canUse() {
-                return MailerEntity.this.getTarget()!=null && MailerEntity.this.getRandom().nextFloat() < 0.4 && MailerEntity.this.distanceToSqr(MailerEntity.this.getTarget()) < 4;
+                return MailerEntity.this.getTarget() != null && MailerEntity.this.getRandom().nextFloat() < 0.4
+                        && MailerEntity.this.distanceToSqr(MailerEntity.this.getTarget()) < 4;
             }
+
             @Override
             public boolean canContinueToUse() {
                 return false;
             }
+
             @Override
             public void start() {
                 super.start();
-                if(MailerEntity.this.level().isClientSide())return;
+                if (MailerEntity.this.level().isClientSide())
+                    return;
                 ItemStack stack = new ItemStack(Items.WRITTEN_BOOK);
                 CompoundTag tag = new CompoundTag();
                 tag.putString(WrittenBookItem.TAG_TITLE, "Invitation");
                 tag.putString(WrittenBookItem.TAG_AUTHOR, "Universal Peacekeepers Organization");
                 ListTag listtag = new ListTag();
                 String s = Component.Serializer.toJson(
-                    Component.literal(String.format("""
-                        Hello %s,\n
-                        we hope this letter finds you well, 
-                        we are the Universal Peacekeepers Organization or UPO for short, 
-                        we are an multi-world Intergovernmental Organization who works""", 
-                        MailerEntity.this.getTarget().getName().getString()
-                    ))
-                );
+                        Component.literal(String.format("""
+                                Hello %s,\n
+                                we hope this letter finds you well,
+                                we are the Universal Peacekeepers Organization or UPO for short,
+                                we are an multi-world Intergovernmental Organization who works""",
+                                MailerEntity.this.getTarget().getName().getString())));
                 String s2 = Component.Serializer.toJson(
-                    Component.literal("""
-                        to protect the peace of this universe,\n
-                        but in the recent years,\n
-                        an war has broken out in the world %d, we, in behalf of this world,\n
-                        ask you  to join us in our mission
-                        """
-                    )
-                );
-                String s3 = Component.Serializer.toJson(
-                    Component.literal("""
-                        to keep the peace of this world, if you are interested, please run /enlist,\n
-                        thank you very much,\n
-                        Universal Peacekeepers Organization
-                        """
-                    )
-                );
+                        Component.literal(String.format("""
+                                to protect the peace of this universe,\n
+                                but in the recent years,\n
+                                an war has broken out in the world %d, we, in behalf of this world,\n
+                                ask you  to join us in our mission
+                                """, MailerEntity.this.getRandom().nextInt(
+                                    1000,
+                                    696969 // gomennasai
+                                ))));
+                String s3 = Component.Serializer.toJson(Component
+                        .literal("to keep the peace of this world, if you are interested, please ")
+                        .append(Component.literal("enlist")
+                                .withStyle(style -> style.applyFormat(ChatFormatting.BLUE)
+                                        .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/enlist"))
+                                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                                                Component.literal("Click to enlist")))))
+                        .append(Component.literal(",\nthank you very much,\nUniversal Peacekeepers Organization")));
+
                 listtag.add(StringTag.valueOf(s));
                 listtag.add(StringTag.valueOf(s2));
                 listtag.add(StringTag.valueOf(s3));
                 tag.put("pages", listtag);
                 stack.save(tag);
                 stack.setTag(tag);
-                ItemEntity en = new ItemEntity(MailerEntity.this.level(), MailerEntity.this.getX(), MailerEntity.this.getY(), MailerEntity.this.getZ(), stack);
+                ItemEntity en = new ItemEntity(MailerEntity.this.level(), MailerEntity.this.getX(),
+                        MailerEntity.this.getY(), MailerEntity.this.getZ(), stack);
                 MailerEntity.this.level().addFreshEntity(en);
                 MailerEntity.this.remove(RemovalReason.DISCARDED);
             }
